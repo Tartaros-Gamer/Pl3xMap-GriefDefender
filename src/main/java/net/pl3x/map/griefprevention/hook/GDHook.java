3dee711e639af3aa5f31cd456a793559924fda77
ahdg <36436808+ahdg6@users.noreply.github.com>
2021-03-11 23:17:42 +0800

    Update and rename GPHook.java to GDHook.java

--- src/main/java/net/pl3x/map/griefprevention/hook/GDHook.java
+++ src/main/java/net/pl3x/map/griefprevention/hook/GDHook.java
@@ -1,20 +1,20 @@
 package net.pl3x.map.griefprevention.hook;
 
-import me.ryanhamshire.GriefPrevention.Claim;
-import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.Core;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
 import java.util.Collection;
 import java.util.UUID;
 
-public class GPHook {
+public class GDHook {
     public static boolean isWorldEnabled(UUID uuid) {
         World world = Bukkit.getWorld(uuid);
-        return GriefPrevention.instance.claimsEnabledForWorld(world);
+        return GriefDefender.getCore().isEnabled(world);
     }
 
     public static Collection<Claim> getClaims() {
-        return GriefPrevention.instance.dataStore.getClaims();
+        return GriefDefender.getCore().getAllClaims();
     }
 }
